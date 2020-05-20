851ef380af566daea0b9c9070540040d229f8cb0
Ashu <ashishdalvi3@gmail.com>
2020-05-20 11:53:16 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+## Main file for namaste otp
+
+import random as r
+
+
+# function for otp generation
+def otpgen():
+        otp=""
+        for i in range(4):
+            otp+=str(r.randint(1,9))
+        print ("Your One Time Password is ")
+        print (otp)
+
+otpgen()
