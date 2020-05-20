61a77fb1c5a5f4b634ee7e1c5f8722f9faf40450
Ashu <ashishdalvi3@gmail.com>
2020-05-20 23:58:14 +0530

    Remove duplicate code

--- app/views.py
+++ app/views.py
@@ -32,22 +32,3 @@ def profile(request):
             userData['following'] = data['following']
         parsedData.append(userData)
     return render(request, 'app/profile.html', {'data': parsedData})
-
-    
-def profile_1(request):
-    jsonList = []
-    req = requests.get('https://api.github.com/users/DrkSephy')
-    jsonList.append(json.loads(req.content))
-    parsedData = []
-    userData = {}
-    for data in jsonList:
-        userData['name'] = data['name']
-        userData['blog'] = data['blog']
-        userData['email'] = data['email']
-        userData['public_gists'] = data['public_gists']
-        userData['public_repos'] = data['public_repos']
-        userData['avatar_url'] = data['avatar_url']
-        userData['followers'] = data['followers']
-        userData['following'] = data['following']
-    parsedData.append(userData)
-    return render(request, 'app/profile.html', {'data': parsedData})
